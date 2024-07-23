a485242b4d9f56babf3385ea8761bcab8170e9a8
YasminTXJ <yasminteixeiradejesus@gmail.com>
2024-07-23 10:03:06 -0300

    tela inicial do gui de viagem versao 1

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -24,45 +24,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.v(TAG, "Visão criada")
         val btLogin = findViewById<Button>(R.id.buttonCont)
-        progressBar = findViewById(R.id.progressBar);
-        val human = findViewById<TextView>(R.id.checkBox)
-        data = findViewById(R.id.date_picker)
+
         //Agora os EVENTOS
-        val imageButton = findViewById<ImageButton>(R.id.imageButton);
-        imageButton.setOnClickListener {
-            if(progressBar.progress == 50){
-                progressBar.progress = 100;
-                Log.d("ImageButton", "Clicou no ImageButton");
-            }else{
-                if(progressBar.progress == 0){
-                    progressBar.progress = 50
-                };
-            }
-        }
-        human.setOnClickListener {
-            if(progressBar.getProgress() == 50){
-                progressBar.progress = 100;
-            }else{
-                if(progressBar.progress == 0){
-                    progressBar.progress = 50
-                };
-            }
-        }
+
+
         btLogin.setOnClickListener {
 
-            val tLogin = findViewById<TextView>(R.id.editTextLogin)
-            val login = tLogin.text.toString()
-            if (login == "yasmin") {
-                val intent = Intent(context, BemVindoActivity::class.java)
-                val params = Bundle()
-                params.putString("nome", "Alexandre")
-                intent.putExtras(params)
-                startActivity(intent)
 
-                //toast("Bem vindo, login realizado com sucesso")//
-            } else {
-                toast("Login incorreto!!")
-            }
+            val intent = Intent(context, BemVindoActivity::class.java)
+            val params = Bundle()
+            params.putString("nome", "Alexandre")
+            intent.putExtras(params)
+            startActivity(intent)
+
+
         }
 
     }
